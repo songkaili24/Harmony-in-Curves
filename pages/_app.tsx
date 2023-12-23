a257b296798050dc70ddcf18f5b72300580b1a42
Kelly Song <kyliewudaonft@gmail.com>
2023-12-23 11:48:02 +0800

    Revert app tsx
    Failed deployment earlier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,9 +33,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:title" content={headerText} />
         <meta
           property="og:description"
-          content="An algorithmic art project based on mathematics to draw seemingly random curves"
+          content="Website is based on MarkSackerbers work"
         />
-        <meta name="description" content="An algorithmic art project based on mathematics to draw seemingly random curves" />
+        <meta name="description" content="Website is based on MarkSackerbers work" />
 
         <meta
           property="og:image"
